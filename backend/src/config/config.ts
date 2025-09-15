67b0ed98d2d18799795beca7ef9ad50878a3caf6
lakhan <lakhanlal3011@gmail.com>
2025-09-15 17:27:21 +0530

    Remove sensitive config.ts from repo and update .gitignore

--- backend/src/config/config.ts
+++
@@ -1,3 +0,0 @@
-export const JWT_SECRET = 'lakhan@123';
-export const BASE_URL = 'localhost:3000';
-export const MONGODB_URI = "mongodb+srv://lakhan:tEKmlacaHr6bFjnf@cluster0.tfjskpg.mongodb.net/brain";
